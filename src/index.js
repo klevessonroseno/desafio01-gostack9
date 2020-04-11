6f69a3ea395f9599afef6968f3be04c10ef31b3b
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-11 12:23:38 -0300

    rota para criar um novo projeto

--- src/index.js
+++ src/index.js
@@ -7,11 +7,19 @@ const projects = [];
 app.use(express.json());
 
 function checkIfExistsProjects(req, res, next){
-    if(!projects[0]) return res.status(404).json({ message: 'Not projects found' });
+    if(!projects[0]) return res.status(404).json({
+        message: 'Not projects found. create a project using POST HTTP method'
+    });
 
     return next();
 }
 
 app.get('/projects', checkIfExistsProjects, (req, res) => res.json(projects));
+
+app.post('/projects', (req, res) => {
+    const { id, title, tasks } = req.body;
+    projects.push(new Project(id, title, tasks));
+    res.status(201).json(projects);
+});
 
 app.listen(port, () => console.log(`Server at port ${port}`));
--- src/models/Projects.js
+++ src/models/Projects.js
@@ -2,7 +2,7 @@ class Project{
     constructor(_id, _title, _tasks){
         this.id = _id;
         this.title = _title;
-        this.tasks = _tasks
+        this.tasks = _tasks || [];
     }
 }
 
