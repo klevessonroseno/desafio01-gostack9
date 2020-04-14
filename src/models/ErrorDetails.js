b224a52a7750f386d4facd3bf381cee9f5299d15
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-14 18:49:55 -0300

    Melhorando a visualização do código da class ErrorDetails

--- src/models/ErrorDetails.js
+++ src/models/ErrorDetails.js
@@ -8,10 +8,26 @@ class ErrorDetails {
 }
 
 const errorDetails = [
-    new ErrorDetails('This project does not exist', 404, new Date(), 'Create a new project using the http post method'),
-    new ErrorDetails('There are no registered projects', 404, new Date(), 'Create a new project using the http post method'),
-    new ErrorDetails('This project already exists', 409, new Date(), 'Error caused by id conflict'),
-    new ErrorDetails('The id, title and tasks attributes are required', 400, new Date(), 'It is necessary to pass id, title and tasks in the request body')
+    new ErrorDetails(
+        'This project does not exist',
+        404, new Date(),
+        'Create a new project using the http post method'
+        ),
+    new ErrorDetails(
+        'There are no registered projects',
+        404, new Date(),
+        'Create a new project using the http post method'
+        ),
+    new ErrorDetails(
+        'This project already exists',
+        409, new Date(),
+        'Error caused by id conflict'
+        ),
+    new ErrorDetails(
+        'The id, title and tasks attributes are required',
+        400, new Date(),
+        'It is necessary to pass id, title and tasks in the request body'
+        )
 ];
 
 export default errorDetails;
